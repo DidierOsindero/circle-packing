73061686e8dcca833109f36b3bac6f245291b561
Didier Osindero <didierosindero@gmail.com>
2022-11-03 11:32:51 +0000

    Format

--- sketch/circlePacking.ts
+++ sketch/circlePacking.ts
@@ -22,7 +22,7 @@ function calculatePackedCircles(
   for (let i = 0; i < 1000; i++) {
     const candidate: CircleData = {
       position: { x: random(0, areaWidth), y: random(0, areaHeight) },
-      radius: random(1,40)
+      radius: random(1, 40),
     };
 
     if (noOverlap(validateCircles, candidate)) {
@@ -31,7 +31,8 @@ function calculatePackedCircles(
   }
   return validateCircles;
 }
-
+/** Checks if circles overlap.
+ */
 function noOverlap(
   _validateCircles: CircleData[],
   _candidate: CircleData
@@ -46,25 +47,6 @@ function noOverlap(
   }
   return true;
 }
-/*
-function calculatePackedCircles(): CircleData[]
-
-	VALIDATED_CIRCLES = empty array of CircleData objects
-	
-	repeat 1000 times
-	
-			CANDIDATE = generate a CircleData object with a random position and radius
-	
-			if CANDIDATE does not overlap* with any element of VALIDATED_CIRCLES
-	
-			    add CANDIDATE to VALIDATED_CIRCLES
-	
-			else
-	
-			    do nothing (we will not use this candidate)
-	
-	return VALIDATED_CIRCLES
-*/
 
 /** Returns the distance between two given positions.
     This function doesn't require the p5.js library.
@@ -75,15 +57,3 @@ function distance(p1: Position, p2: Position): number {
   const hyp = Math.sqrt(x * x + y * y);
   return hyp;
 }
-
-//TODO: you need to implement this function properly!
-//These are just a couple of random circles, with no consideration yet for avoiding overlap.
-// const circleDataOne: CircleData = {
-//     position: { x: 300, y: 300 },
-//     radius: 100,
-// };
-// const circleDataTwo: CircleData = {
-//     position: { x: random(0, areaWidth), y: random(0, areaHeight) },
-//     radius: 40,
-// };
-// return [circleDataOne, circleDataTwo];
